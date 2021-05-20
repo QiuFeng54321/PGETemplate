76b7d67c65dd971f3c030899986092744451a51b
15000714579 <yuheng.ye@ulink.com>
2021-05-20 23:44:19 +0800

    Add license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+//     <one line to give the program's name and a brief idea of what it does.>
+//     Copyright (C) 2021  Qiufeng54321
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "olcPixelGameEngine.h"
 #include "Resources.h"
 #include "ScreenManager.h"
@@ -45,12 +61,12 @@ int main() {
 
     if (!williamcraft::SoundEngine)
         std::cout << "Failed to initialize the engine" << std::endl; // error starting up the engine
-    irrKlangResourceFileFactory* factory = new irrKlangResourceFileFactory();
+    auto factory = new irrKlangResourceFileFactory();
 
 
 //    williamcraft::SoundEngine->addFileFactory(factory);
     std::filesystem::current_path("/Users/mac/Documents/VSCProjects/PGETemplate/");
-    std::string key = "";
+    std::string key;
 
     williamcraft::ResourcePack = new olc::ResourcePack();
     williamcraft::ResourcePack->LoadPack("ResourcePack.dat", key);
